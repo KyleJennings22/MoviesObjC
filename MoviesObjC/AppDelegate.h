e95f4038072a87d29321ab737ec18b11d458ad24
Kyle Jennings <Kyle22@gmail.com>
2019-12-06 09:02:50 -0700

    initial commit

---
+++ MoviesObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoviesObjC
+//
+//  Created by Kyle Jennings on 12/6/19.
+//  Copyright © 2019 Kyle Jennings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MoviesObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MoviesObjC
+//
+//  Created by Kyle Jennings on 12/6/19.
+//  Copyright © 2019 Kyle Jennings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
