b8607e3edbece9b3af54f9aabe2da252a8e686aa
Kyle Jennings <Kyle22@gmail.com>
2019-12-06 11:26:46 -0700

    finish black diamonds

--- MoviesObjC/View Controller/MovieTableViewController.swift
+++ MoviesObjC/View Controller/MovieTableViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class MovieTableViewController: UITableViewController, UISearchBarDelegate {
-
+    
     // MARK: - Outlets
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var favoritesBarButtonItem: UIBarButtonItem!
     
     
     // MARK: - Properties
@@ -24,7 +25,15 @@ class MovieTableViewController: UITableViewController, UISearchBarDelegate {
         }
     }
     
+    // empty array to hold our favorite movies
     var favoriteMovies: [KPJMovie] = []
+    
+    // property observer to see if favorites button is on or off
+    var favoriteIsOn: Bool? {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
     
     // MARK: - Lifecycle Functions
     override func viewDidLoad() {
@@ -32,27 +41,71 @@ class MovieTableViewController: UITableViewController, UISearchBarDelegate {
         // cant forget to set delegate
         searchBar.delegate = self
     }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // since movies is optional, if it contains nil, set numberofrows to 0
-        return movies?.count ?? 0
+    
+    // MARK: - Actions
+    @IBAction func favoritesButtonTapped(_ sender: UIBarButtonItem) {
+        if favoriteIsOn == nil {
+            favoriteIsOn = true
+            favoritesBarButtonItem.title = "Cancel"
+        } else if favoriteIsOn == true {
+            favoriteIsOn = false
+            favoritesBarButtonItem.title = "Favorites"
+        } else if favoriteIsOn == false {
+            favoriteIsOn = true
+            favoritesBarButtonItem.title = "Cancel"
+        }
     }
-
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if favoriteIsOn == true {
+            // returning the count of favorite movies instead of movies
+            return favoriteMovies.count
+        } else {
+            // since movies is optional, if it contains nil, set numberofrows to 0
+            return movies?.count ?? 0
+        }
+    }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // casting our cell as our custom MovieTableViewCell
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieTableViewCell else {return UITableViewCell()}
-
-        // grabbing the movie from the index
-        let movie = movies?[indexPath.row]
-        // sending the movie to the custom cell to update the views
-        cell.movie = movie
-
+        
+        if favoriteIsOn == true {
+            // grabbing the movie from the index of favoriteMovies
+            let movie = favoriteMovies[indexPath.row]
+            // sending the movie to the custom cell to update the views
+            cell.movie = movie
+        } else {
+            // grabbing the movie from the index of movies
+            let movie = movies?[indexPath.row]
+            // sending the movie to the custom cell to update the views
+            cell.movie = movie
+        }
+        
         return cell
     }
     
+    // MARK: - Editable Table View Cells Function
+    
+    // I know this is deprecated but I couldn't find a different way to do this
+    override func tableView(_ tableView: UITableView, editActionsForRowAt: IndexPath) -> [UITableViewRowAction]? {
+
+        // creating our favorite action
+        let favorite = UITableViewRowAction(style: .normal, title: "Favorite") { action, index in
+            // creating a movie from our movies index
+            guard let movie = self.movies?[index.row] else {return}
+            // appending the movie to the favorite movie array
+            self.favoriteMovies.append(movie)
+        }
+        favorite.backgroundColor = .orange
+
+        return [favorite]
+    }
+   
     // MARK: - SearchBar Functions
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         // unwrapping the searchbar text to be sure it exists and isn't empty
@@ -68,7 +121,7 @@ class MovieTableViewController: UITableViewController, UISearchBarDelegate {
     }
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toMovieDetail" {
